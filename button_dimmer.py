0f16da49cc9b8b34baf128ae80ad33d2ce8d592f
Phil <philmon22@users.noreply.github.com>
2025-12-27 13:13:32 -0500

    Refactor button dimmer script to implement separate buttons for brightness control and adjust LED brightness dynamically

--- button_dimmer.py
+++ button_dimmer.py
@@ -4,35 +4,50 @@ import time
 # Set up GPIO
 GPIO.cleanup()  # Clean up any previous GPIO settings
 GPIO.setmode(GPIO.BOARD)
-BUTTON_PIN = 40
+BUTTON_BRIGHTER_PIN = 40
+BUTTON_DIMMER_PIN = 36
 LED_PIN = 35
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON_BRIGHTER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BUTTON_DIMMER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(LED_PIN, GPIO.OUT)
-dimmer=GPIO.PWM(LED_PIN, 100)
-dimmer.start(10)
-time.sleep(2)
-dimmer.ChangeDutyCycle(40)
-time.sleep(2)
-dimmer.ChangeDutyCycle(80)
-time.sleep(2)
- 
- 
-
+led=GPIO.PWM(LED_PIN, 100)
+led.start(0)
 
 #used for button press
-# button_pressed = False
-# while True:
-#     # Read button state
-#     button_state = GPIO.input(BUTTON_PIN)
-    
-#     if button_state == GPIO.LOW:  # Button pressed
-#         if not button_pressed:
-#             GPIO.output(LED_PIN, GPIO.HIGH)  # Turn LED ON
-#             print("Button pressed! LED ON")
-#             button_pressed = True
-#             time.sleep(0.5)  # Debounce delay
-#         else:
-#             GPIO.output(LED_PIN, GPIO.LOW)  # Turn LED OFF
-#             print("Button released! LED OFF")
-#             button_pressed = False
+button_level = 0
+
+# track previous states to detect edges (only trigger once per press)
+brighter_prev = GPIO.HIGH
+dimmer_prev = GPIO.HIGH
+
+while True:
+    # Read button state
+    brighter_state = GPIO.input(BUTTON_BRIGHTER_PIN)
+    dimmer_state = GPIO.input(BUTTON_DIMMER_PIN)
+
+    # Detect press event: transition HIGH -> LOW
+    if brighter_state == GPIO.LOW and brighter_prev == GPIO.HIGH:
+        print("Brighter button pressed")
+        if button_level < 100:
+            button_level = min(100, button_level + 20)
+            led.ChangeDutyCycle(button_level)
+        else:
+            print("LED is at maximum brightness")
+        time.sleep(0.05)  # short debounce
+
+    if dimmer_state == GPIO.LOW and dimmer_prev == GPIO.HIGH:
+        print("Dimmer button pressed")
+        if button_level > 0:
+            button_level = max(0, button_level - 20)
+            led.ChangeDutyCycle(button_level)
+        else:
+            print("LED is at minimum brightness")
+        time.sleep(0.05)  # short debounce
+
+    # save states for next iteration
+    brighter_prev = brighter_state
+    dimmer_prev = dimmer_state
+
+    time.sleep(0.01)  # small loop delay
+ 
     
